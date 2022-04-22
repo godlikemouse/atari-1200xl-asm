eff96c8fda7c563fe0b378d1ddecbb2bc5702cb6
Jason Graves <godlikemouse@godlikemouse.com>
2022-04-22 00:13:14 -0600

    Interim checkin for .proc between refactor

--- dungeoneer/src/util.asm
+++ dungeoneer/src/util.asm
@@ -39,3 +39,46 @@ done
     tax
     lda TMP0
 .endm
+
+;
+; between
+;   compares low <= value < high
+;   stores temporarily TMP0,TMP1,TMP2,TMP3
+;   result stored in acc
+.proc bet (.byte low+1, value+1, high+1) .var
+low mva #0 TMP1
+value mva #0 TMP2
+high mva #0 TMP3
+
+    ;save stack
+    txa
+    pha
+
+    lda TMP2
+    ldx TMP1
+    stx TMP0
+
+    ;compare acc to low end
+    cmp TMP0
+    bcc fail
+
+    ;compare acc to high end
+    ldx TMP3
+    stx TMP0
+    cmp TMP0
+    bcs fail
+    lda #1
+    sta TMP0
+    jmp done
+
+fail
+    lda #0
+    sta TMP0
+
+done
+	;restore stack
+	pla
+	tax
+	lda TMP0
+	rts
+.endp
