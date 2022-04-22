2b2c26d46b72e67c0c4c5e3c8668a6efa48f2800
Jason Graves <jgraves@datto.com>
2022-04-22 08:52:08 -0600

    Updated todo

--- dungeoneer/src/dungeoneer.asm
+++ dungeoneer/src/dungeoneer.asm
@@ -4,6 +4,10 @@
 ; NTSC Color Palette: https://atariage.com/forums/uploads/monthly_10_2015/post-6369-0-47505700-1443889945.png
 ; PMG Memory Map: https://www.atarimagazines.com/compute/issue64/atari_animation.gif
 
+; TODO: add door open sound
+; TODO: add exit level sound
+; TODO: add main menu sound / background music
+; TODO: add intro sound
 ; TODO: add door with key interaction
 ; TODO: add end of level interaction
 ; TODO: add transfer tile functionality
