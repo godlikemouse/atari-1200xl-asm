eb80cfd0fb25f382da8e2ee2dde13177328d73b6
Jason Graves <jgraves@datto.com>
2022-04-25 09:22:03 -0600

    between proc refactor, restored level1

--- dungeoneer/src/util.asm
+++ dungeoneer/src/util.asm
@@ -7,12 +7,13 @@
 ;   stores temporarily TMP0,TMP6,TMP7,TMP8
 ;   result stored in acc
 .proc between (.byte low+1, value+1, high+1) .var
-low mva #0 TMP6
-value mva #0 TMP7
-high mva #0 TMP8
-_low=TMP6
-_value=TMP7
-_high=TMP8
+.var _low .byte
+.var _value .byte
+.var _high .byte
+.var _temp .byte
+low mva #0 _low
+value mva #0 _value
+high mva #0 _high
 
     ;save stack
     txa
@@ -20,30 +21,30 @@ _high=TMP8
 
     lda _value
     ldx _low
-    stx TMP0
+    stx _temp
 
     ;compare acc to low end
-    cmp TMP0
+    cmp _temp
     bcc fail
 
     ;compare acc to high end
     ldx _high
-    stx TMP0
-    cmp TMP0
+    stx _temp
+    cmp _temp
     bcs fail
     lda #1
-    sta TMP0
+    sta _temp
     jmp done
 
 fail
     lda #0
-    sta TMP0
+    sta _temp
 
 done
 	;restore stack
 	pla
 	tax
-	lda TMP0
+	lda _temp
 	rts
 .endp
 
