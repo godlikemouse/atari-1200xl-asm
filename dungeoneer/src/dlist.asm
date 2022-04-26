140811e1ca3d8b6acabf1892bcc01f534433472e
Jason Graves <godlikemouse@godlikemouse.com>
2022-04-26 01:03:53 -0600

    Minor refactor, added screen DLI

--- dungeoneer/src/dlist.asm
+++ dungeoneer/src/dlist.asm
@@ -5,43 +5,51 @@
 ; setup display list for screen
 ;
 .proc setup_game_screen
-blank8=$70 ; 8 blank lines
-blank7=$60 ; 7 blank lines
-LMS=$40 ; Load Memory Scan (LMS)
-JVB=$41 ; Jump while vertical blank (JVB)
-antic5=5 ; ANTIC mode 5
-antic2=2 ; ANTIC mode 2
+BLANK8=$70 ; 8 blank lines
+BLANK7=$60 ; 7 blank lines
+LMS=$40 ; load memory scan (LMS)
+JVB=$41 ; jump vertical black (JVB)
+DLI=$80 ; display list interrupt
+ANTIC5=$05 ; ANTIC mode 5
 
 	mwa #dlist SDLSTL ; move word takes care of high/low byte assignments
 	rts
 
 ; display list
 dlist
-	.byte blank8, blank8, blank7
-	.byte antic5 + LMS, <item_screen, >item_screen
-	.byte antic5 + LMS, <GAME_SCREEN, >GAME_SCREEN
-	.byte antic5, antic5, antic5, antic5, antic5, antic5
-	.byte antic5, antic5, antic5, antic5, antic5
-	.byte JVB, <dlist, >dlist
+	.byte BLANK8, BLANK8, BLANK7
+	.byte DLI + LMS + ANTIC5
+	.word item_screen
+	.byte DLI + LMS + ANTIC5
+	.word GAME_SCREEN
+	.byte DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5
+	.byte DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5
+	.byte DLI + ANTIC5
+	.byte JVB
+	.word dlist
 .endp
 
 ;
 ; setup game over / menu screen
 ;
 .proc setup_menu_screen
-blank8=$70 ; 8 blank lines
-LMS=$40 ; Load Memory Scan (LMS)
-JVB=$41 ; Jump while vertical blank (JVB)
-antic5=5 ; ANTIC mode 5
+BLANK8=$70 ; 8 blank lines
+LMS=$40 ; load memory scan (LMS)
+JVB=$41 ; jump vertical blank (JVB)
+DLI=$80 ; display list interrupt
+ANTIC5=5 ; ANTIC mode 5
 
 	mwa #dlist SDLSTL ; move word takes care of high/low byte assignments
 	rts
 
 ; display list
 dlist
-	.byte blank8, blank8, blank8
-	.byte antic5 + LMS, <MENU_SCREEN, >MENU_SCREEN
-	.byte antic5, antic5, antic5, antic5, antic5, antic5
-	.byte antic5, antic5, antic5, antic5, antic5
-	.byte JVB, <dlist, >dlist
+	.byte BLANK8, BLANK8, BLANK8
+	.byte ANTIC5 + LMS
+	.word MENU_SCREEN
+	.byte DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5
+	.byte DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5, DLI + ANTIC5
+	.byte DLI + ANTIC5
+	.byte JVB
+	.word dlist
 .endp
