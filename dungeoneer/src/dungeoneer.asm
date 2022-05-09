f79c0590bb3dff348837c4487213110d4f377234
Jason Graves <jgraves@datto.com>
2022-05-09 08:58:44 -0600

    Updated todo list

--- dungeoneer/src/dungeoneer.asm
+++ dungeoneer/src/dungeoneer.asm
@@ -20,7 +20,6 @@
 ; TODO: add extra life gain
 ; TODO: fix spider invisible on win game issue
 ; TODO: fix spider slow after fourth win issue
-; TODO: add pass behind for secret walls
 ; TODO: fix win sound timing
 
 	org $0600
